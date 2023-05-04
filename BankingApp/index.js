2e5beafe0437d87416f42ba7968fdc7195426dc8
OMAR CERDA <115116330+CenturionTech@users.noreply.github.com>
2023-05-04 17:22:14 -0600

    Update index.js

--- BankingApp/index.js
+++ BankingApp/index.js
@@ -1,21 +1,36 @@
-function Spa() {
-  return (
-    <HashRouter>
-      <NavBar/>
-      <UserContext.Provider value={{users:[{name:'Omar Cerda',email:'int3lltec.group@gmail.com',password:'Topsecret',balance:1000}]}}>
-        <div className="container" style={{padding: "20px"}}>
-          <Route path="/" exact component={Home} />
-          <Route path="/CreateAccount/" component={CreateAccount} />
-          <Route path="/deposit/" component={Deposit} />
-          <Route path="/withdraw/" component={Withdraw} />
-          <Route path="/alldata/" component={AllData} />
-        </div>
-      </UserContext.Provider>      
-    </HashRouter>
-  );
-}
+"use strict";
 
-ReactDOM.render(
-  <Spa/>,
-  document.getElementById('root')
-);
+function Spa() {
+  return /*#__PURE__*/React.createElement(HashRouter, null, /*#__PURE__*/React.createElement(NavBar, null), /*#__PURE__*/React.createElement(UserContext.Provider, {
+    value: {
+      users: [{
+        name: 'Omar Cerda',
+        email: 'int3lltec.group@gmail.com',
+        password: 'Topsecret',
+        balance: 1000
+      }]
+    }
+  }, /*#__PURE__*/React.createElement("div", {
+    className: "container",
+    style: {
+      padding: "20px"
+    }
+  }, /*#__PURE__*/React.createElement(Route, {
+    path: "/",
+    exact: true,
+    component: Home
+  }), /*#__PURE__*/React.createElement(Route, {
+    path: "/CreateAccount/",
+    component: CreateAccount
+  }), /*#__PURE__*/React.createElement(Route, {
+    path: "/deposit/",
+    component: Deposit
+  }), /*#__PURE__*/React.createElement(Route, {
+    path: "/withdraw/",
+    component: Withdraw
+  }), /*#__PURE__*/React.createElement(Route, {
+    path: "/alldata/",
+    component: AllData
+  }))));
+}
+ReactDOM.render( /*#__PURE__*/React.createElement(Spa, null), document.getElementById('root'));
