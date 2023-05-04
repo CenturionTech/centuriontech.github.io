ea02ee512bc100887649ef131cabc584b6be2e1d
OMAR CERDA <115116330+CenturionTech@users.noreply.github.com>
2023-05-04 17:18:53 -0600

    Update alldata.js

--- BankingApp/alldata.js
+++ BankingApp/alldata.js
@@ -1,31 +1,29 @@
-function AllData() {
-  const ctx = React.useContext(UserContext);
+"use strict";
 
-  return (
-    <div className="container">
-      <h5 className="mb-4">All Data</h5>
-      <table className="table">
-        <thead>
-          <tr>
-            <th scope="col">#</th>
-            <th scope="col">Name</th>
-            <th scope="col">Email</th>
-            <th scope="col">Password</th>
-            <th scope="col">Balance</th>
-          </tr>
-        </thead>
-        <tbody>
-          {ctx.users.map((user, index) => (
-            <tr key={user.email} className={index % 2 === 0 ? "table-primary" : "table-secondary"}>
-              <th scope="row">{index + 1}</th>
-              <td>{user.name}</td>
-              <td>{user.email}</td>
-              <td>{user.password}</td>
-              <td>{user.balance}</td>
-            </tr>
-          ))}
-        </tbody>
-      </table>
-    </div>
-  );
+function AllData() {
+  var ctx = React.useContext(UserContext);
+  return /*#__PURE__*/React.createElement("div", {
+    className: "container"
+  }, /*#__PURE__*/React.createElement("h5", {
+    className: "mb-4"
+  }, "All Data"), /*#__PURE__*/React.createElement("table", {
+    className: "table"
+  }, /*#__PURE__*/React.createElement("thead", null, /*#__PURE__*/React.createElement("tr", null, /*#__PURE__*/React.createElement("th", {
+    scope: "col"
+  }, "#"), /*#__PURE__*/React.createElement("th", {
+    scope: "col"
+  }, "Name"), /*#__PURE__*/React.createElement("th", {
+    scope: "col"
+  }, "Email"), /*#__PURE__*/React.createElement("th", {
+    scope: "col"
+  }, "Password"), /*#__PURE__*/React.createElement("th", {
+    scope: "col"
+  }, "Balance"))), /*#__PURE__*/React.createElement("tbody", null, ctx.users.map(function (user, index) {
+    return /*#__PURE__*/React.createElement("tr", {
+      key: user.email,
+      className: index % 2 === 0 ? "table-primary" : "table-secondary"
+    }, /*#__PURE__*/React.createElement("th", {
+      scope: "row"
+    }, index + 1), /*#__PURE__*/React.createElement("td", null, user.name), /*#__PURE__*/React.createElement("td", null, user.email), /*#__PURE__*/React.createElement("td", null, user.password), /*#__PURE__*/React.createElement("td", null, user.balance));
+  }))));
 }
