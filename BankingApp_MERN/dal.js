0b3fa0bf18386ad7f4be3c590e7db747944131c8
OMAR CERDA <115116330+CenturionTech@users.noreply.github.com>
2023-07-27 13:06:53 -0600

    Add files via upload

---
+++ BankingApp_MERN/dal.js
@@ -0,0 +1,103 @@
+const MongoClient = require('mongodb').MongoClient;
+const url         = 'mongodb://localhost:27017';
+//const url           = 'mongodb://doadmin:H0p4h9L3T2mo7Z56@myproject-109eca8f.mongo.ondigitalocean.com:27017/admin?';
+let db            = null;
+ 
+// connect to mongo
+MongoClient.connect(url, {useUnifiedTopology: true}, function(err, client) {
+    console.log("Connected successfully to db server");
+
+    // connect to myproject database
+    db = client.db('myproject');
+});
+
+// create user account
+function create(name, email, password){
+    return new Promise((resolve, reject) => {    
+        const collection = db.collection('users');
+        const doc = {name, email, password, balance: 0};
+        collection.insertOne(doc, {w:1}, function(err, result) {
+            err ? reject(err) : resolve(doc);
+        });    
+    })
+}
+
+// create transaction
+function transaction(email, typeTrans, amount){
+    return new Promise((resolve, reject) => {    
+        const collection = db.collection('transactions');
+        const dateTime   = new Date();
+        const doc = {dateTime, email, typeTrans, amount};
+        collection.insertOne(doc, {w:1}, function(err, result) {
+            err ? reject(err) : resolve(doc);
+        });    
+    })
+}
+
+// find user account
+function find(email){
+    return new Promise((resolve, reject) => {    
+        const customers = db
+            .collection('users')
+            .find({email: email})
+            .toArray(function(err, docs) {
+                err ? reject(err) : resolve(docs);
+        });    
+    })
+}
+
+// find user account
+function findOne(email){
+    return new Promise((resolve, reject) => {    
+        const customers = db
+            .collection('users')
+            .findOne({email: email})
+            .then((doc) => resolve(doc))
+            .catch((err) => reject(err));    
+    })
+}
+
+// update - deposit/withdraw amount
+function update(email, amount){
+    return new Promise((resolve, reject) => {    
+        const customers = db
+            .collection('users')            
+            .findOneAndUpdate(
+                {email: email},
+                { $inc: { balance: amount}},
+                { returnOriginal: false },
+                function (err, documents) {
+                    err ? reject(err) : resolve(documents);
+                }
+            );            
+
+
+    });    
+}
+
+// all users
+function all(){
+    return new Promise((resolve, reject) => {    
+        const customers = db
+            .collection('users')
+            .find({})
+            .toArray(function(err, docs) {
+                err ? reject(err) : resolve(docs);
+        });    
+    })
+}
+
+// transactions
+function transactions(){
+    return new Promise((resolve, reject) => {    
+        const customers = db
+            .collection('transactions')
+            .find({})
+            .toArray(function(err, docs) {
+                err ? reject(err) : resolve(docs);
+        });    
+    })
+}
+
+
+module.exports = {create, transaction, findOne, find, update, all, transactions};
